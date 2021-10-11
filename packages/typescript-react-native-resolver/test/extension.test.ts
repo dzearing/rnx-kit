a1e39854443dbf58839d73f6d37c95af4c53206a
Adam Foxman <afoxman@microsoft.com>
2021-10-11 16:41:44 -0700

    create a test suite for typescript-react-native-resolver and add an empty test

---
+++ packages/typescript-react-native-resolver/test/extension.test.ts
@@ -0,0 +1,14 @@
+import "jest-extended";
+import { hasExtension, getExtensionFromPath } from "../src/extension";
+
+describe("Extension > hasExtension", () => {
+  test("", () => {
+    // nop
+  });
+});
+
+describe("Extension > getExtensionFromPath", () => {
+  test("", () => {
+    // nop
+  });
+});
