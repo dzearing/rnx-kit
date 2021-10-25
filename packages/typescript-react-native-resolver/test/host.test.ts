4d1b6e4059d78b75d7554c54d77c9861dc18ee6b
Adam Foxman <afoxman@microsoft.com>
2021-10-25 11:05:06 -0700

    local versioning for private testing, empty host tests

---
+++ packages/typescript-react-native-resolver/test/host.test.ts
@@ -0,0 +1,31 @@
+import "jest-extended";
+import {
+  changeCompilerHostToUseReactNativeResolver,
+  resolveModuleName,
+  resolveModuleNames,
+  resolveTypeReferenceDirectives,
+} from "../src/host";
+
+describe("Host > changeCompilerHostToUseReactNativeResolver", () => {
+  test("", () => {
+    // nop
+  });
+});
+
+describe("Host > resolveModuleName", () => {
+  test("", () => {
+    // nop
+  });
+});
+
+describe("Host > resolveModuleNames", () => {
+  test("", () => {
+    // nop
+  });
+});
+
+describe("Host > resolveTypeReferenceDirectives", () => {
+  test("", () => {
+    // nop
+  });
+});
